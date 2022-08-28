346a5e834d68d54a1fca24201542d9a6474d9d9e
jancy <jotabbusiness@icloud.com>
2022-08-27 20:44:22 -0500

    firebase sign up

--- report-card/src/App.tsx
+++ report-card/src/App.tsx
@@ -1,27 +1,94 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
+import { firebaseConfig } from './Firebase';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
 
 function App() {
-  const [URL, setURL] = useState([])
+  // get username
+  const [newUser, setNewUser] = useState('');
+  // get email
+  const [password, setPassword] = useState('');
 
-
-  const url = 'http://localhost:5001/report-card-for-people/us-central1/helloWorld';
-
+  // need to create a aysnc await
+  // save email to firebase
+  const userInfo = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, newUser, password)
+        .then((userCrendtials => {
+          const user = userCrendtials.user
+          console.log(user)
+          if (newUser.length > 0) {
+            addDoc(collection(db, 'users'), {
+              id: user.uid,
+              user: user.email,
+            })
+          }
+        }))
+    } catch (err) {
+      console.log(err)
+      alert(err)
+    }
+  }
 
   useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then(res => setURL(res))
+    console.log('here we are')
   }, [])
 
 
   return (
     <div className="App">
-      <div>
-        { URL }
+      <div className='box'>
+        <p className='header'>Welcome to the PAGE of NOTHINGNESSSSS</p>
+        {/* userName input */}
+        <input
+          className='user'
+          type='text'
+          name='user'
+          placeholder='Enter UserName'
+          onChange={(e) => {
+            setNewUser(e.target.value)
+            console.log(newUser)
+          }
+          }
+        />
+        {/* password */}
+        <input
+          className='password'
+          type='password'
+          placeholder='Enter Password'
+          onChange={(e) =>
+            setPassword(e.target.value)
+          }
+        />
+        {/* signup button */}
+        <input
+          type='submit'
+          value='Sign-up'
+          className='btn pagelink'
+          placeholder='signup'
+          onClick={() => userInfo()}
+        />
+        {/* signin button */}
+        <input
+          className='btn singin'
+          type='submit'
+          value='Signin'
+        // onClick={() => go to signin page }
+        />
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+//auth.CreateUserWithEmailAndPasswordAsync(email, password)
---
+++ report-card/src/Firebase.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAcBonYatsg_Jf38u64i0nSX_4I3S4V7xE",
+    authDomain: "report-card-for-people.firebaseapp.com",
+    projectId: "report-card-for-people",
+    storageBucket: "report-card-for-people.appspot.com",
+    messagingSenderId: "277839983730",
+    appId: "1:277839983730:web:69fd9528b34832e2c3beeb",
+    measurementId: "G-YTPMYG5JEZ"
+  };
